815766bfd74ce18f225798062b80291ba1f97202
dave <andreazzini.davide@gmail.com>
2017-11-23 22:15:34 +0000

    changes

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.json({version: 1}))
+  .get('/api', (req, res) => res.json({version: 1}))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
