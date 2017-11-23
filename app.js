eebf4d4bf8977234a5d5d70d313f2283a641a7fd
dave <andreazzini.davide@gmail.com>
2017-11-23 23:41:29 +0000

    dim

--- app.js
+++ app.js
@@ -48,10 +48,13 @@ app.post('/api/voice', (req, res) => {
 
 app.post('/api/voice/dim', (req, res) => {
   let direction = req.body.direction.toLowerCase()
+  console.log(direction)
   let val = direction=="down" ? -50 : 50;
+  
   state.color.b= Math.max(0,Math.min(state.color.b+val,255))
   state.color.r= Math.max(0,Math.min(state.color.r+val,255))
   state.color.g= Math.max(0,Math.min(state.color.g+val,255))
+
   console.log(state)
   res.json(color)
 })
