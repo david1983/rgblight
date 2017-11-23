4f0333e21f8d27e797ec8034d0d29b059c980e91
dave <andreazzini.davide@gmail.com>
2017-11-23 23:25:36 +0000

    changes

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 let state = {"color":{"r":0,"g":244,"b":255,"a":1}}
 
 const colors = {
+  default: {r:10, g: 10, b: 10, a:1},
   Yellow: {r: 251, g: 255, b: 0, a: 1},
   Red: {r: 255, g: 0, b: 0, a: 1},
   Green: {r: 0, g: 255, b: 0, a: 1},
@@ -37,7 +38,10 @@ app.post('/api', (req, res) => {
 
 app.post('/api/voice', (req, res) => {
   let color = req.body.color
-  state.color = colors[color] || colors.Yellow
+  state.color = colors[color] || colors.default
+  state.name = color
+
+  console.log(state)
   res.json(color)
 })
 
