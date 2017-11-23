9636f3fbba38e427f66873ebcd6ff2b05879c015
dave <andreazzini.davide@gmail.com>
2017-11-23 22:38:27 +0000

    post

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const cors = require("cors")
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .get('/api', (req, res) => res.json({version: 1}))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(cors())
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+let state = {}
+
+app.get('/api', (req, res) => {
+
+  res.json(state)
+})
+app.get('/post', (req, res) => {
+
+  console.log(req.body)
+  res.json({version: req.body})
+})
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
