a8772b622963fe48d59d01b706872ee1c6c1ec09
dave <andreazzini.davide@gmail.com>
2017-11-23 22:09:55 +0000

    changes

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.use(cors)
-
-app.get("/", (req,res)=>{
-    res.json({version: 1})
-})
-
-app.listen(8080)
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (req, res) => res.json({version: 1}))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
